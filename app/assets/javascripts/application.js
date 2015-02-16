9206a91743d183eb98729b2ed2b918e118fac7fc
Bryan Knouse <bryan.knouse@gmail.com>
2015-02-16 15:54:59 -0500

    styling/logos

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,19 +16,23 @@
 
 
 
-// Scroll
+// Main Doc Ready Function
+$(document).ready(function() {
 
-$('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        || location.hostname == this.hostname) {
+    // Smooth scrolling Javascript
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+            || location.hostname == this.hostname) {
 
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-           if (target.length) {
-             $('html,body').animate({
-                 scrollTop: target.offset().top + 5
-            }, 750);
-            return false;
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+               if (target.length) {
+                 $('html,body').animate({
+                     scrollTop: target.offset().top - 150
+                }, 750);
+                return false;
+            }
         }
-    }
+    });
 });
