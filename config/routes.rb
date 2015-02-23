09df286dcb21ba5f56b180dd233c365b64e1133b
Bryan Knouse <bryan.knouse@gmail.com>
2015-02-23 17:29:06 -0500

    under construction

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   match '/signup', to: 'users#new', via: 'get'
  
   get 'main/index'
+  get 'main/home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
