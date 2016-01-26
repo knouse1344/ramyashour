c37a80c5333d0acfcf338d7bd19d167a927efb5b
Bryan Knouse <bryan.knouse@gmail.com>
2016-01-26 12:51:47 -0500

    updated ramy gemspec

--- config/routes.rb
+++ config/routes.rb
@@ -13,13 +13,14 @@ Rails.application.routes.draw do
  
   get 'main/index'
   get 'main/home'
+  get 'main/2016'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
 
-  root 'main#index'
+  root 'main#2016'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
